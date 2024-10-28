692178926e39ef4282647a864f98fdf76788c5ab
G4B0CM <Gabriel.calderon.matute@udla.edu.ec>
2024-10-28 07:40:17 -0500

    Primer Modelo

---
+++ GabrielCalderon_ExamenProgreso1/Models/GCalderon.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GabrielCalderon_ExamenProgreso1.Models
+{
+    public class GCalderon
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [StringLength(50)]
+        [AllowNull]
+        public string Apellido { get; set; }
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [StringLength(50)]
+        public string Contrasena { get; set; }
+        [Required]
+        [Phone]
+        public string numtelf { get; set; }
+        [Required]
+        public double Saldo { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+        [Required]
+        public bool EsCliente { get; set; }
+
+
+    }
+}
